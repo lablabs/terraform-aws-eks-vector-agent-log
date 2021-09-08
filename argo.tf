76d882b60ebf854cbbdfff0e25fe5cb0fdda6cc9
Martin Hauskrecht <hauskrechtmartin@gmail.com>
2021-09-08 16:59:34 +0200

    Fix values merge to argo

--- argo.tf
+++ argo.tf
@@ -16,7 +16,7 @@ resource "kubernetes_manifest" "this" {
         "helm" = {
           "releaseName" = var.helm_release_name
           "parameters"  = [for k, v in var.settings : tomap({ "forceString" : true, "name" : k, "value" : v })]
-          "values"      = var.values
+          "values"      = data.utils_deep_merge_yaml.values[0].output
         }
       }
       "destination" = {
